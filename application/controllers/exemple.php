836c6c21f129e902c97601fc6724b9a0d6653ec9
Yoann.Vanitou <Yoann.Vanitou@PC-Yoann.zenith.local>
2014-06-11 17:18:41 +0200

    Version 2.9

---
+++ application/controllers/exemple.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Exemple extends CI_Controller {
+	
+	public function __construct() {
+		parent::__construct();
+		
+		$this->load->database();
+		$this->load->library('orm');
+	}
+	
+	public function index() {
+		
+		// Exemple 1
+		$product = new product_model(1);			
+		$product->price = 10.10;
+		$product->save();
+		
+		// Exemple 2
+		$product = new product_model();
+		$product->where('id', 1)->find_one();
+		$product->price = 10.10;
+		$product->save();
+		
+		// Exemple 3
+		$product = new product_model(3);			
+		$image = $product->product_image->find();
+		echo $image->file;
+		
+		// Exemple 4
+		$product = new product_model(3);			
+		$comments = $product->product_comment->find();
+		
+		foreach ($comments as $comment) {
+			echo $comment->title;
+		}
+	}
+
+}
+
+/* End of file Exemple.php */
+/* Location: ./application/controllers/Exemple.php */
