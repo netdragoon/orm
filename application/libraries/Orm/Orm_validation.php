14ce161832473a73629966f8995cf2e8396caf66
malty <malty@192.168.1.100>
2014-06-23 21:44:17 +0200

    Implementation du validator

--- application/libraries/Orm/Orm_validation.php
+++ application/libraries/Orm/Orm_validation.php
@@ -9,28 +9,94 @@
  */
 class Orm_validation {
 
-    const TYPE_EMAIL = 'email';
-    const TYPE_URL = 'url';
-    const TYPE_IP = 'ip';
-    const TYPE_EXCLUSION = 'exclusion';
-    const TYPE_FORMAT = 'format';
-    const TYPE_INCLUSION = 'inclusion';
-    const TYPE_LENGTH = 'length';
-    const TYPE_PRESENCE = 'presence';
-    const TYPE_CALLBACK = 'callback';
+    const OPTION_TYPE_EMAIL = 'email';
+    const OPTION_TYPE_URL = 'url';
+    const OPTION_TYPE_IP = 'ip';
+    const OPTION_TYPE_EXCLUSION = 'exclusion';
+    const OPTION_TYPE_FORMAT = 'format';
+    const OPTION_TYPE_INCLUSION = 'inclusion';
+    const OPTION_TYPE_LENGTH = 'length';
+    const OPTION_TYPE_PRESENCE = 'presence';
+    const OPTION_TYPE_CALLBACK = 'callback';
     const OPTION_MIN = 'min';
     const OPTION_MAX = 'max';
     const OPTION_LIST = 'list';
     const OPTION_MATCHER = 'matcher';
 
     public $field;
-    public $value;
     public $type;
+    public $min;
+    public $max;
+    public $list;
+    public $matcher;
 
     public function __construct(array $data) {
-        $this->field = $data['field'];
-        $this->value = $data['value'];
-        $this->type = $data['type'];
+        
+        foreach ($data as $key => $value) {
+            if (isset($this->{$key})) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+    
+    private function _email($value) {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+
+    private function _url($value) {
+        return filter_var($value, FILTER_VALIDATE_URL);
+    }
+
+    private function _ip($value) {
+        return filter_var($value, FILTER_VALIDATE_IP);
+    }
+
+    private function _exclusion($value) {
+        return !in_array($value, $config);
+    }
+
+    private function _format($value) {
+        preg_match($config, $value);
+    }
+
+    private function _inclusion($value) {
+        return in_array($value, $config);
+    }
+
+    private function _length($value) {
+        if (empty($value))
+            return FALSE;
+
+        $length = strlen($value);
+
+        if (($length < $config['min']) || ($length > $config['max'])) {
+            return FALSE;
+        } else {
+            return $value;
+        }
+    }
+
+    private function _presence($value) {
+        if (empty($value) && $value !== 0) {
+            return FALSE;
+        } else {
+            return $value;
+        }
+    }
+
+    private function _callback($value) {
+        return all_user_func($value, $config);
+    }
+    
+    public function validate(Orm_field $field) {
+        $method = "_$this->type";
+        
+        if ($this->$method($field->value) === FALSE) {
+            return FALSE;
+        }
+        
+        return TRUE;
+        
     }
 }
 
