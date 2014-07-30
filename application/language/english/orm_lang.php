a62d66679a65fff1dd2216fdbafdb1e68ae9f8de
Yoann.Vanitou <Yoann.Vanitou@PC-Yoann.zenith.local>
2014-07-30 16:02:11 +0200

    3.2.1 (20140729)

--- application/language/english/orm_lang.php
+++ application/language/english/orm_lang.php
@@ -7,17 +7,17 @@
  * @version 3.2.1 (20140729)
  */
 
-$lang['orm_validation_email'] = '';
-$lang['orm_validation_url'] = '';
-$lang['orm_validation_ip'] = '';
-$lang['orm_validation_int'] = '';
-$lang['orm_validation_float'] = '';
-$lang['orm_validation_exclusion'] = '';
-$lang['orm_validation_inclusion'] = '';
-$lang['orm_validation_format'] = '';
-$lang['orm_validation_length'] = '';
-$lang['orm_validation_presence'] = '';
-$lang['orm_validation_callback'] = 'Le champ %s est requis';
+$lang['orm_validation_email'] = "Le champ %s doit être un email valide";
+$lang['orm_validation_url'] = "Le champ %s doit être une URL valide";
+$lang['orm_validation_ip'] = "Le champ %s doit être une IP valide";
+$lang['orm_validation_int'] = "Le champ %s doit être un nombre entier";
+$lang['orm_validation_float'] = "Le champ %s doit être un nombre a virgule";
+$lang['orm_validation_exclusion'] = "Le champ %s n'a pas la bonne valeur";
+$lang['orm_validation_inclusion'] = "Le champ %s n'a pas la bonne valeur";
+$lang['orm_validation_format'] = "Le champ %s n'a pas la bonne valeur";
+$lang['orm_validation_date'] = "Le champ %s n'est pas une date valide";
+$lang['orm_validation_length'] = "Le champ %s n'a pas la bonne valeur";
+$lang['orm_validation_presence'] = 'Le champ %s est requis';
 
 /* End of file orm_lang_lang.php */
 /* Location: ./application/language/english/orm_lang_lang.php */
